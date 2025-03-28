79ed68fdbc6aa13e8f42fa7f7d5e79c4e3ee1444
rikut0904 <rkt9tuba4robot@gmail.com>
2025-03-28 23:23:08 +0900

    create:Map表示

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,17 @@
-import Flutter
 import UIKit
+import Flutter
+import GoogleMaps
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    GMSServices.provideAPIKey(Bundle.main.object(forInfoDictionaryKey: "MAPS_API_KEY") as! String)
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
--- lib/UI/map.dart
+++ lib/UI/map.dart
@@ -1,10 +1,54 @@
-import "package:flutter/material.dart";
+// ignore_for_file: depend_on_referenced_packages
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
   const MapPage({super.key});
 
   @override
+  MapPageState createState() => MapPageState();
+}
+
+class MapPageState extends State<MapPage> with AutomaticKeepAliveClientMixin {
+  GoogleMapController? mapController;
+  bool _isMapRendered = false;
+  bool _isMapCreated = false;
+
+  @override
+  bool get wantKeepAlive => true;
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+    setState(() {
+      _isMapCreated = true;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    super.build(context);
+    return Stack(
+      children: [
+        GoogleMap(
+          onMapCreated: _onMapCreated,
+          onCameraIdle: () {
+            if (_isMapCreated && !_isMapRendered) {
+              Future.delayed(const Duration(seconds: 6), () {
+                if (mounted) {
+                  setState(() {
+                    _isMapRendered = true;
+                  });
+                }
+              });
+            }
+          },
+          initialCameraPosition: CameraPosition(
+            target: LatLng(35.681236, 139.767125),
+            zoom: 14,
+          ),
+        ),
+        if (!_isMapRendered) const Center(child: CircularProgressIndicator()),
+      ],
+    );
   }
 }
