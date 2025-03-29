68093c32faa1997bc64fd6de8f2603fa720553b3
rikut0904 <rkt9tuba4robot@gmail.com>
2025-03-30 04:31:40 +0900

    add:現在地取得

--- lib/UI/map.dart
+++ lib/UI/map.dart
@@ -1,53 +1,135 @@
-// ignore_for_file: depend_on_referenced_packages
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:drivepay/logic/map.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
 
   @override
-  MapPageState createState() => MapPageState();
+  State<MapPage> createState() => _MapPageState();
 }
 
-class MapPageState extends State<MapPage> with AutomaticKeepAliveClientMixin {
+class _MapPageState extends State<MapPage> with AutomaticKeepAliveClientMixin {
   GoogleMapController? mapController;
-  bool _isMapRendered = false;
-  bool _isMapCreated = false;
+  CameraPosition? _initialLocation;
+  Set<Marker> _markers = {};
+  late MapLogic _mapLogic;
+  final TextEditingController _searchController = TextEditingController();
+  final TextEditingController _startAddressController = TextEditingController();
 
   @override
   bool get wantKeepAlive => true;
 
+  @override
+  void initState() {
+    super.initState();
+
+    _mapLogic = MapLogic(
+      mapController: mapController,
+      updateMarkers: (newMarkers) {
+        setState(() {
+          _markers = newMarkers;
+        });
+      },
+      updateStartAddress: (address) {
+        setState(() {
+          _startAddressController.text = address;
+        });
+      },
+    );
+
+    _mapLogic.getCurrentLocation((cameraPosition) {
+      setState(() {
+        _initialLocation = cameraPosition;
+      });
+    });
+  }
+
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
-    setState(() {
-      _isMapCreated = true;
+    _mapLogic.setMapController(controller);
+
+    _mapLogic.getCurrentLocation((cameraPosition) {
+      setState(() {
+        _initialLocation = cameraPosition;
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
     super.build(context);
+
+    if (_initialLocation == null) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
     return Stack(
       children: [
-        GoogleMap(
-          onMapCreated: _onMapCreated,
-          onCameraIdle: () {
-            if (_isMapCreated && !_isMapRendered) {
-              Future.delayed(const Duration(seconds: 6), () {
-                if (mounted) {
-                  setState(() {
-                    _isMapRendered = true;
-                  });
-                }
-              });
-            }
-          },
-          initialCameraPosition: CameraPosition(
-            target: LatLng(35.681236, 139.767125),
-            zoom: 14,
-          ),
+        Stack(
+          children: [
+            GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: _initialLocation!,
+              myLocationEnabled: true,
+              myLocationButtonEnabled: false,
+              markers: _markers,
+              mapType: MapType.normal,
+              zoomControlsEnabled: false,
+            ),
+            Positioned(
+              top: 10,
+              left: 16,
+              right: 16,
+              child: Column(
+                children: [
+                  Material(
+                    elevation: 4.0,
+                    borderRadius: BorderRadius.circular(8),
+                    child: TextField(
+                      controller: _searchController,
+                      decoration: const InputDecoration(
+                        hintText: '場所を検索',
+                        prefixIcon: Icon(Icons.search),
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.all(12),
+                      ),
+                      onSubmitted: (query) {
+                        _mapLogic.searchPlace(query);
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  ElevatedButton(
+                    onPressed: () {
+                      _mapLogic.searchNearbyGasStations();
+                    },
+                    child: const Text('近くのガソリンスタンドを探す'),
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 10,
+              right: 16,
+              child: ClipOval(
+                child: Material(
+                  color: Colors.white,
+                  child: InkWell(
+                    child: SizedBox(
+                      width: 56,
+                      height: 56,
+                      child: Icon(Icons.my_location),
+                    ),
+                    onTap: () {
+                      _mapLogic.moveToCurrentLocation(context);
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
-        if (!_isMapRendered) const Center(child: CircularProgressIndicator()),
       ],
     );
   }
