8370d01924537ac12272c62dd25f26afa79023c9
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2025-04-02 00:01:17 +0900

    add: iconとラベルを追加

--- lib/UI/home.dart
+++ lib/UI/home.dart
@@ -33,6 +33,20 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Row(
+              children: [
+                const Icon(
+                  Icons.location_on,
+                  color: Color(0xFFdf5656),
+                  size: 24,
+                ),
+                const Text(
+                  '現在地から出発',
+                  style: TextStyle(color: Color(0xFF45C4B0)),
+                ),
+              ],
+            ),
+            const SizedBox(height: 2),
             InputText(
               label: '出発地',
               hintText: '駅、バス停、住所、施設',
