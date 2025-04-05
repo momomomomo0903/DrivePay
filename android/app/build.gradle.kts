0b52945e3278c7044fa9a1630ffe664328485ff3
rikut0904 <rkt9tuba4robot@gmail.com>
2025-04-05 22:27:44 +0900

    firebase対応

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.drivepay"
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
