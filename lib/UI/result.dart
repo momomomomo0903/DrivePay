f9c7474a6e4639d45cb409aad10a9c1d87af8de8
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2025-04-23 23:40:50 +0900

    feat: UIを修正し、金額コピー機能を追加

--- lib/UI/result.dart
+++ lib/UI/result.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: unnecessary_brace_in_string_interps
 
+import 'package:drivepay/UI/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class ResultPage extends StatelessWidget {
   final int perPersonAmount;
@@ -21,52 +23,158 @@ class ResultPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              width: double.infinity,
-              height: 350,
-              padding: const EdgeInsets.all(20),
-              decoration: const BoxDecoration(
-                color: Color(0xFF4EC1B7),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.only(top: 100, bottom: 150),
+                decoration: const BoxDecoration(color: Color(0xFF45C4B0)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      '¥$perPersonAmount',
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 70,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+                    // アイコン＋テキストを縦に並べる
+                    Column(
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.link, color: Colors.white),
+                          iconSize: 40,
+                          padding: EdgeInsets.zero, // アイコン周りの余白をなくす
+                          constraints: const BoxConstraints(), // 最小サイズ制限を解除
+                          onPressed: () {
+                            Clipboard.setData(
+                              ClipboardData(text: perPersonAmount.toString()),
+                            );
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text('コピーしました')),
+                            );
+                          },
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(left: 8),
+                          child: Text(
+                            '金額をコピー',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12, // 小さめに
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-              child: Column(
+              const SizedBox(height: 100),
+              Column(
+                mainAxisSize: MainAxisSize.min,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  const Icon(Icons.directions_car, color: Colors.white, size: 30),
-                  const SizedBox(height: 10),
-                  const Text(
-                    'Drive Pay',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  const Text(
-                    '一人あたり',
-                    style: TextStyle(color: Colors.white, fontSize: 14),
-                  ),
-                  const SizedBox(height: 100),
-                  Text(
-                    '¥$perPersonAmount',
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 70,
-                      fontWeight: FontWeight.bold,
-                    ),
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        width: 300,
+                        alignment: Alignment.centerLeft,
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 8,
+                          horizontal: 16,
+                        ),
+                        decoration: const BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(
+                              color: Color.fromARGB(255, 192, 192, 192),
+                            ),
+                          ),
+                        ),
+                        child: Text(
+                          '距離　　　　${distance.toStringAsFixed(1)} Km',
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xff8F8F8F),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 300,
+                        alignment: Alignment.centerLeft,
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 8,
+                          horizontal: 16,
+                        ),
+                        decoration: const BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(color: Colors.grey),
+                          ),
+                        ),
+                        child: Text(
+                          '一人　　　　${perPersonAmount}円 × ${peopleCount}人',
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xff8F8F8F),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 300,
+                        alignment: Alignment.centerLeft,
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 8,
+                          horizontal: 16,
+                        ),
+                        child: Text(
+                          '合計　　　　${totalAmount}円',
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xff8F8F8F),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 50),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          elevation: 5,
+                          shape: const StadiumBorder(),
+                          padding: const EdgeInsets.only(
+                            left: 70.0,
+                            right: 70.0,
+                          ),
+                          backgroundColor: const Color(0xFF45C4B0),
+                          foregroundColor: Colors.white,
+                        ),
+                        onPressed: () {
+                          Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const HomePage(),
+                            ),
+                            (route) => false,
+                          );
+                        },
+                        child: const Text('ホームに戻る'),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ),
-            const SizedBox(height: 100),
-            Text('距離　　　　${distance.toStringAsFixed(1)} Km', style: const TextStyle(fontSize: 16)),
-            Text('一人　　　　${perPersonAmount}円 × ${peopleCount}人', style: const TextStyle(fontSize: 16)),
-            Text('合計　　　　${totalAmount}円', style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-          ],
+            ],
+          ),
         ),
       ),
     );
-  }}
+  }
+}
